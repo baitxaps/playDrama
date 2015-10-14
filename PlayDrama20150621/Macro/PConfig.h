b7383c8b397d978160b226379e33213d722d2bd3
rhc <baitxaps@126.com>
2015-10-14 17:21:24 +0800

    Macro

--- PlayDrama20150621/Macro/PConfig.h
+++ PlayDrama20150621/Macro/PConfig.h
@@ -68,24 +68,15 @@
 
 /******* GO DEBUG LOG ********/
 #if DEBUG
-#define PDebugLog(frmt, ...)   {NSLog((frmt), ##__VA_ARGS__);}
-
-#ifdef USE_NSLOGGER
-#define LoggerStream(level, ...)   LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Stream", level, __VA_ARGS__)
-#define LoggerVideo(level,  ...)   LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Video",  level, __VA_ARGS__)
-#define LoggerAudio(level,  ...)   LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Audio",  level, __VA_ARGS__)
+#    define PDebugLog(frmt, ...)
+#    define LoggerStream(level, ...)   NSLog(__VA_ARGS__)
+#    define LoggerVideo(level, ...)    NSLog(__VA_ARGS__)
+#    define LoggerAudio(level, ...)    NSLog(__VA_ARGS__)
 #else
-#define LoggerStream(level, ...)   NSLog(__VA_ARGS__)
-#define LoggerVideo(level,  ...)   NSLog(__VA_ARGS__)
-#define LoggerAudio(level,  ...)   NSLog(__VA_ARGS__)
-#endif
-
-#else
-#define PDebugLog(frmt, ...)
-
-#define LoggerStream(...)           while(0) {}
-#define LoggerVideo (...)           while(0) {}
-#define LoggerAudio (...)           while(0) {}
+#    define PDebugLog(frmt, ...)
+#    define LoggerStream(...)          while(0) {}
+#    define LoggerVideo(...)           while(0) {}
+#    define LoggerAudio(...)           while(0) {}
 #endif
 /*******  DEBUG LOG ********/
 
